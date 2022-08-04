35130cf50f0d3ee6a41c0008b4bb9be0bf761042
FabioMartins <fabbio1000@gmail.com>
2022-08-04 16:25:28 -0300

    Feat: Criando validações para o endpoint /login

--- index.js
+++ index.js
@@ -51,10 +51,21 @@ app.get('/talker/:id', async (req, res) => {
 
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
+  const verifyEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   
-  if (!email || !password) {
-   return res.status(400).json({ message: 'Login inválido' });
+  if (!email) {
+   return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
+  if (!verifyEmail.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
   const token = crypto.randomBytes(8).toString('hex');
   return res.status(200).json({ token });
 });
