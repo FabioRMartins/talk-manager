e74c049c5d663e3880b285aaefe5a8be69c5c6d8
FabioMartins <fabbio1000@gmail.com>
2022-08-04 14:13:09 -0300

    Feat: Criando endpoint GET /talker

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const TALKER_JSON = './talker.json';
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +17,22 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+const readTalkers = async () => {
+  try {
+    const response = await fs.readFile(TALKER_JSON, 'utf-8');
+    const talkerList = JSON.parse(response);
+    return talkerList;  
+  } catch (e) {
+    return 'erro';
+  }
+};
+
+app.get('/talker', async (req, res) => {
+  const talkers = await readTalkers();
+  if (talkers.length > 0) {
+    return res.status(200).json(talkers);
+  } 
+  return res.status(200).json([]);
 });
